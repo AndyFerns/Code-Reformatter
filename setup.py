258fa7dff6f54041d382ca42cd380a087221b7bf
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-19 12:34:02 +0530

    removed cache run script

--- setup.py
+++ setup.py
@@ -2,20 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='code_formatter_cli',
-    version='1.0',
+    version='1.1.0',
     packages=find_packages(),
     install_requires=[
         'python-docx'
     ],
     entry_points={
         'console_scripts': [
-            'formatcode=cli_tool.main:run'
+            'formatcode=cli_tool.main:main'
         ]
     },
     author='Andrew Fernandes',
     description='CLI tool to format code with output in txt, docx, or LaTeX formats',
     classifiers=[
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+        'Intended Audience :: Developers'
     ],
     python_requires='>=3.6'
 )
